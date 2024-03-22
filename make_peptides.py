437b489b08835cafc4e150e60703bf8e0dee6ef9
Steven Tavis <stavis@vols.utk.edu>
2024-03-22 14:57:07 -0400

    refactored peptide rollup to account for all-mzml processing in parse_mzml.py

--- make_peptides.py
+++ make_peptides.py
@@ -8,8 +8,10 @@ Created on Thu Mar 21 17:02:02 2024
 
 from argparse import ArgumentParser
 parser = ArgumentParser()
-parser.add_argument('-p', '--psms', action = 'append', required = True,
-                    help = 'Use once per .dill file created by parse_mzml.py')
+parser.add_argument('-p', '--psms', action = 'store', required = True,
+                    help = 'The .dill file created by parse_mzml.py')
+parser.add_argument('-d', '--design', action = 'store', required = True,
+                    help = 'The experimental design file')
 parser.add_argument('-o','--out', action = 'store', required = True,
                     help = 'File name for results.')
 args = parser.parse_args()
@@ -18,25 +20,17 @@ from collections import defaultdict
 import dill
 from tools.fitting_tools import peptide
 
-labels = []
-psms = []
-for file in args.psms:
-    with open(file, 'rb') as dillfile:
-        psms.extend(dill.load(dillfile))
-    labels.append(psms[-1].label)
-
-labels = set(labels)
-if len(labels) > 1:
-    raise Exception('Multiple labels found. This script should only be used to combine replicates.')
+with open(args.psms, 'rb') as dillfile:
+    psms = dill.load(dillfile)
 
 def fingerprint(psm):
-    return (psm.sequence, psm.mods)
+    return (psm.sequence, psm.mods, psm.label, *psm.metadata.values())
 
 psm_lists = defaultdict(lambda:[])
 for psm in psms:
     psm_lists[fingerprint(psm)].append(psm)
 
-peptides = [peptide(psm_list) for psm_list in psm_lists]
+peptides = [peptide(psm_list) for psm_list in psm_lists.values()]
 
 with open(args.out, 'wb') as dillfile:
     dill.dump(peptides, dillfile)
